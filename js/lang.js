5fe86f560dd248576f6690b8f331bdd6df864854
Cleaveit <lele@lele-air.local>
2016-11-22 15:32:56 +0100

    Cleaveit Social Network
    commit by RatLab

---
+++ js/lang.js
@@ -0,0 +1,148 @@
+// ----------------------------
+// Multilang
+// ----------------------------
+
+
+var arrLang = {
+    			'it' : {
+    				't1' : 'La WebApp per qualsiasi progetto',
+                    
+                    't2' : 'Un altro Social Network?!',
+                    
+                    'p1' : 'Cleaveit è una piattaforma web opensource, basata sulla condivisione del sapere sia accademico che universitario, in grado di far combaciare arte e scienza, esperienza e tecnica. La piattaforma sarà in grado di ospitare qualsiasi tipo di progetto e cercherà di abbracciare più arti/settori possibili, avvicinando mondi simili ma diversi che abitano in una ambito comune. Il “Cleave’it” è il punto nevralgico, attraverso esso puoi decidere se aderire o meno ad un progetto ad un pensiero condiviso o in più generale ad una notizia.',
+                    
+                    't3' : 'Prova le versione Beta di Cleaveit!',
+                    
+                    't3a' : 'Ti invieremo la credenziali di accesso il più presto possibile!',
+                    
+                    't4' : 'Caratteristiche',
+                    
+                    't4a' : 'Che cosa vogliamo costruire - Che cosa vogliamo includere',
+                    
+                    'p2' : 'Vogliamo costruire una piattaforma web dove tutti possono condividere i propri progetti in qualsiasi formato.',
+                    
+                    'p3' : 'Uno dei punti cardine dell’applicazione web, sarà l’ottimizzazione cross-browser compatibility e la ricerca dell\'accessibilità completa.',
+                    
+                    'p4' : 'La piattaforma nasce open source a livello di codice, per questo tutti possono contribuire al progetto',
+                    
+                    't5' : 'Per saperne di più..',
+                    
+                    't5a' : 'Iniziare con noi è semplice per chiunque',
+                    
+                    't6' : 'A chi è rivolto?',
+                    
+                    't6a' : 'Cleaveit è rivolto a tutti coloro che frequentano un corso di studi Accademico/Universitario e che hanno intenzione di condividere le proprie idee e progetti.',
+                    
+                    't7' : 'Come posso partecipare al progetto?',
+                    
+                    't7a' : 'Cleaveit nasce opensource, quindi tutti possono partecipare allo sviluppo del progetto, contattaci!',
+                    
+                    't8' : 'Che ruolo posso avere in Cleaveit?',
+                    
+                    't8a' : ' Qualsiasi, cerchiamo grafici, programmatori, artisti, video maker, sound designer ecc ecc..',
+                    
+                    't9' : 'Chi siamo?',
+                    
+                    't10' : 'Vuoi sviluppare Cleaveit con noi?' 
+    				
+    			},
+
+    			'en' : {
+    				't1' : 'The WebApp for any project',
+                    
+                    't2' : 'Another Social Network?!',
+                    
+                    'p1' : 'Cleaveit is an opensource web platform, based on academic knowledge sharing which is able to match art and science, experience and technique. The Platform will be able to host any type of project and it will embrace any kind of art, connecting similar (but different) branches at the same time.  “Cleave’it” is the central point, through it you can decide to join a project, a shared belief or news.',
+                    
+                    't3' : 'Try the Beta version of Cleaveit!',
+                    
+                    't3a' : 'We will send a message with your credentials as soon as possible!',
+                    
+                    't4' : 'Features',
+                    
+                    't4a' : 'What we want include - What we want build',
+                    
+                    'p2' : 'We want to build a web platform in which everyone can share any kind of project.',
+                    
+                    'p3' : 'One of the key point of this platform will be the cross-browser compatibility. We are also trying to achieve a complete accessibility. ',
+                    
+                    'p4' : 'Cleaveit was born as an open source project, for this reason everyone can contribute to it',
+                    
+                    't5' : 'Frequently Asked Questions',
+                    
+                    't5a' : 'Start with us is totally easy for everyone',
+                    
+                    't6' : 'Who it’s for',
+                    
+                    't6a' : 'Cleaveit is aimed for anyone who attend a course of academic and University studies and that they plan to share their ideas and projects.',
+                    
+                    't7' : 'How can i join it?',
+                    
+                    't7a' : 'Cleaveit born opensource, so anyone can participate in the development of the project, We want you!',
+                    
+                    't8' : 'What role can have in Cleaveit?',
+                    
+                    't8a' : 'Whatever, we are searching graphic designers, programmers, artists, video makers, sound designers etc etc ..',
+                    
+                    't9' : 'Who we are?',
+                    
+                    't10' : 'Would you like to develop Cleaveit with us?'
+    				
+    			},
+
+                'fr' : {
+                    't1' : 'L’application web pour tous les projets',
+
+                    't2' : 'Un autre Social Network?!',
+                    
+                    'p1' : 'Cleaveit est une plate-forme web opnesource, fondé sur le partage du savoir soit académique que universitaire, en mesure de faire coïncider art et science, expérience et technique. La plate-forme sera en mesure de recevoir n’importe quel type de projet et cherchera d’embrasser plus d’art / de secteurs possibles, en approchant des mondes pareils mais différents qui habitent dans un milieu commun. “Le Cleaveit” est le point névralgique, par celui-ci on peut décider si adhérer ou moins à un projet, à une pensée partagée ou en gènèral à une nouvelle.',
+                    
+                    't3' : 'Testez la version Beta de Cleaveit!',
+                    
+                    't3a' : 'Nous vous enverrons un message avec vos informations d’identification le plus tôt possible!',
+                    
+                    't4' : 'Fonctionnalités',
+                    
+                    't4a' : 'Ce que nous voulons inclure - Ce que nous voulons construire',
+                    
+                    'p2' : 'On veut construire une plate-forme web où tout le monde peut partager ses projets dans n’importe quel format.',
+                    
+                    'p3' : 'Un des points-clés de l’application web sera l’optimisation cross-browser compatibility et la recherche de l’accessibilitè complète. ',
+                    
+                    'p4' : 'La plate-forme naît opensource au niveau de code, pour ce la tout le monde peut contribuer au projet.',
+                    
+                    't5' : 'Questions fréquemment posées',
+                    
+                    't5a' : 'Commencez avec nous est totalement facile pour tout le monde',
+                    
+                    't6' : 'A’ qui est destinè?',
+                    
+                    't6a' : 'Il est destiné à tous ceux qui assistent à un cours d’études universitaires / académique et qu’ils ont l’intention de partager leurs idées et leurs projets.',
+                    
+                    't7' : 'Comment je puis participer?',
+                    
+                    't7a' : 'Cleaveit né open source, donc tout le monde peut participer à l’élaboration du projet, s’il vous plaît contactez-nous!',
+                    
+                    't8' : 'Quel rôle peut avoir dans Cleaveit?',
+                    
+                    't8a' : 'Quoiqu’il en soit, nous recherchons des graphistes, des programmeurs, des artistes, vidéastes, concepteurs sonores, etc etc ..',
+                    
+                    't9' : 'Qui nous sommes?',
+                    
+                    't10' : 'Vous voulez développer Cleaveit avec nous?'
+                    
+                }
+
+    		};
+
+    		$(function(){
+    			$('.translate').click(function(){
+    				var lang = $(this).attr('id');
+
+    				$('.lang').each(function(index, element ){
+    					$(this).text(arrLang[lang][$(this).attr('key')]);
+    				});
+    			});
+    		});
+
+    	
